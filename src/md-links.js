60244b702ffd64936ec9750a390a5d81b9fd9870
Lilian Cardenas <lili.denisse2@gmail.com>
2023-06-21 20:56:04 -0400

    funcionando

--- src/md-links.js
+++ src/md-links.js
@@ -1,15 +1,31 @@
 const { isABsolute, isRelative, isValid, isMarkdown } = require("./path");
 
 function mdLinks(route) {
+   let abs = isABsolute(route);
+   const rel = isRelative(route)
+   const val = isValid(route)
+   const md = isMarkdown(route)
    
-   
-   
-   return {
-      isABsolute: isABsolute(route),
-      isRelative: isRelative(route),
-      isValid: isValid(route),
-      isMarkdown: isMarkdown(route)
-    };
+   let newRoute
+   let result 
+
+   if (abs == false) {
+       newRoute = rel;
+       abs = isABsolute(newRoute);
+   } 
+
+   if ( abs == true && val == true && md == true ) {
+      result = true;
+   } 
+   else {
+      result = false;
+   }
+    
+    return result;
+  
+
 }
 
-console.log(mdLinks('C:\\Users\\56957\\Desktop\\Laboratoria\\MD-LINKS\\DEV006-md-links-lili\\'));
+console.log(mdLinks('lilianmmmm.md'));
+
+//C:/Users/56957/Desktop/Laboratoria/MD-LINKS/DEV006-md-links-lili/lili.md
